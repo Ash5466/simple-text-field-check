5cdaf640ebd4992ef89a6a15958598a513feba49
Ash5466 <ashishthakur7545@gmail.com>
2024-09-10 15:51:58 +0530

    Update custom snippet.php

--- custom snippet.php
+++ custom snippet.php
@@ -2,18 +2,18 @@
 
 function wpf_dev_require_ten_digit_number_any_form( $fields, $entry, $form_data ) {
     
-    // Loop through all fields in the form
+    // checking all fields in form
     foreach ( $fields as $field_id => $field ) {
         
-        // Check if the field is a "Single Line Text" or "Rich Text" field (change 'textarea' if required)
+        // Check if the field is a single line text
         if ( 'text' === $field['type'] ) {
             
             // Get the value of the field
             $mystring = $field['value'];
             
-            // Check if the input is a 10-digit number using regex
+            // Checking if input is a 10-digit number using regex
             if ( !preg_match( '/^\d{10}$/', $mystring ) ) {
-                // Add an error message if the input doesn't match the 10-digit number requirement
+                // Adding error message, if input does not match requirement
                 wpforms()->process->errors[ $form_data['id'] ][ $field_id ] = esc_html__( 'Please enter a valid 10-digit number.', 'wpforms' );
             }
         }
